d4559a5b2bb9e14b28ddfb6eb7f627d1786338a0
John A Ginis <drxspace@gmail.com>
2015-07-19 20:23:10 +0300

    notifies

--- su-filemanager/su-filemanager.sh
+++ su-filemanager/su-filemanager.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
+#
+# _________        ____  ____________         _______ ___________________
+# ______  /__________  |/ /___  ____/________ ___    |__  ____/___  ____/
+# _  __  / __  ___/__    / ______ \  ___  __ \__  /| |_  /     __  __/
+# / /_/ /  _  /    _    |   ____/ /  __  /_/ /_  ___ |/ /___   _  /___
+# \__,_/   /_/     /_/|_|  /_____/   _  .___/ /_/  |_|\____/   /_____/
+#                                    /_/           drxspace@gmail.com
+#
 
 LANG=en_US.UTF-8
 ###
@@ -17,7 +25,7 @@ elif [[ "$DESKTOP_SESSION" =~ ubuntu|gnome* && -x "$(which nautilus)" ]]; then
 elif [[ "$DESKTOP_SESSION" =~ kde* && -x "$(which dolphin)" ]]; then
 	pkexec "$(which dolphin)"
 else
-	notify-send "File Manager" "A known “File Manager” program was not found." -i face-sad &
+	notify-send "File Manager" "I could not detect any of the three “Desktop Environment ≡ File Manager” pairs." -i face-sad &
 	exit 1
 fi
 
