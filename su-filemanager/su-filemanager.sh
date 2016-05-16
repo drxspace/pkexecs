922962e9468627eb36ddbc78b46d8a31a79d7c4e
John A Ginis <drxspace@gmail.com>
2016-05-16 20:21:05 +0300

    caja

--- su-filemanager/su-filemanager.sh
+++ su-filemanager/su-filemanager.sh
@@ -18,15 +18,16 @@ if [[ -z "$XAUTHORITY" ]] && [[ -e "$HOME/.Xauthority" ]]; then
 	export XAUTHORITY="$HOME/.Xauthority"
 fi
 
-
-if [[ "$DESKTOP_SESSION" =~ cinnamon && -x "$(which nemo)" ]]; then
-	pkexec "$(which nemo)"
-elif [[ "$DESKTOP_SESSION" =~ ubuntu|gnome* && -x "$(which nautilus)" ]]; then
+if [[ "$DESKTOP_SESSION" =~ ubuntu|gnome* && -x "$(which nautilus)" ]]; then
 	pkexec "$(which nautilus)"
+elif [[ "$DESKTOP_SESSION" =~ cinnamon && -x "$(which nemo)" ]]; then
+	pkexec "$(which nemo)"
+elif [[ "$DESKTOP_SESSION" =~ mate && -x "$(which caja)" ]]; then
+	pkexec "$(which caja)"
 elif [[ "$DESKTOP_SESSION" =~ kde* && -x "$(which dolphin)" ]]; then
 	pkexec "$(which dolphin)"
 else
-	notify-send "File Manager" "I could not detect any of the three “Desktop Environment ≡ File Manager” pairs." -i face-sad &
+	notify-send "File Manager" "I could not detect any of these “Desktop Environment ≡ File Manager” pairs." -i face-sad &
 	exit 1
 fi
 
